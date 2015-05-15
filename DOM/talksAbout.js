2cd55bb76b934e96559cf8a852a6afc1f2d41e25
Yogi.Jiang <Yogi.Jiang@hotmail.com>
2015-05-16 07:15:05 +0800

    4
    4

---
+++ DOM/talksAbout.js
@@ -0,0 +1,16 @@
+function talksAbout(node, string) {
+    if (node.nodeType == document.ELEMENT_NODE) {
+        for (var i = 0; i < node.childNodes.length; i++) {
+            if(talksAbout(node.childNodes[i], string)){
+                return true;
+            }
+        }
+        return false;
+    } else if (node.nodeType == document.TEXT_NODE) {
+        return node.nodeValue.indexOf(string) > -1;
+    }
+}
+console.log(talksAbout(document.body, "book"));
+/**
+ * Created by Administrator on 2015/5/7.
+ */
