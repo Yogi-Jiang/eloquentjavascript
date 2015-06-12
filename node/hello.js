5b2f22ea0e007d6364e9c497e237aa76223331bd
Yogi.Jiang <Yogi.Jiang@hotmail.com>
2015-06-12 17:26:48 +0800

    15.6.12

---
+++ node/hello.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2015/6/10.
+ */
+var message = "Hello World";
+console.log(message);
---
+++ node/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2015/6/10.
+ */
+var garble = require("./garble");
+
+var argument = process.argv[2];
+console.log(garble(argument));
